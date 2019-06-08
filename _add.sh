70639b2c3d85c8a885030752a47f5743fd9b39d5
sami <kozlosam@gmail.com>
2019-06-08 12:23:21 +0200

    Add tools now checks whether network detected

--- _add.sh
+++ _add.sh
@@ -2,14 +2,33 @@
 
 source "/home/sami/dev/bash/wifi/aux.sh"
 
-if [[ -z $1 || -z $2 ]]; then
-	wifi_log "need SSID and password to add a network"
+if [[ -z $1 ]]; then
+	wifi_log "need to give network's SSID"
 	exit 1
 fi
 
-ssid="$1"
-pass="$2"
+given_ssid="$1"
 
-echo "$ssid:::$pass" | cat - "${wifi_list}" > "temp" && mv "temp" "${wifi_list}" && chmod 000 "${wifi_list}" && wifi_log "successfully added '$ssid'" && exit 0
+while read -r available_ssid; do
+	if [[ ${available_ssid} == ${given_ssid} ]]; then
+	    given_ssid_available_flag=0
+	    break
+    fi
+done <<< $(print_available)
 
-wifi_log "could not add '$ssid'"
+if [[ -z ${given_ssid_available_flag} ]]; then
+    wifi_log "the network '${given_ssid}' is not detected. continue?"
+
+    read -n1 cont
+    printf "\n"
+    if [[ ! ${cont} == 'Y' || ! ${cont} == 'y' ]]; then
+        exit 1
+    fi
+fi
+
+wifi_log "provide password for '${given_ssid}'"
+read -s given_pass
+
+echo "${given_ssid}:::${given_pass}" | cat - "${wifi_list}" > "temp" && mv "temp" "${wifi_list}" && chmod 000 "${wifi_list}" && wifi_log "successfully added '${given_ssid}'" && exit 0
+
+wifi_log "could not add '${given_ssid}'"
