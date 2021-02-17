c7f9946aa4a5a1cbdae2782e425fdcf7b94e42f2
sami <kozlosam@gmail.com>
2021-02-17 23:36:06 +0100

    Connecting to multiple networks
    Implemented disconnect

--- _connect.sh
+++ _connect.sh
@@ -16,33 +16,46 @@ done < "${wifi_list}"
 # load the list of available networks
 
 declare -i available_index=0
-
 while read -r network_line; do
 	available[${available_index}]="${network_line#SSID: }"
 	available_index+=1
-done <<< $(iw dev "$wireless_interface" scan | grep "SSID:")
+done <<< $(iw dev "$wireless_interface" scan | grep "SSID:" | sort -u)
 
 
-# find the first saved among the available
+# find those that are recognised_ssids
 
+declare -i recognised_ssids_index=0
 for (( i=0 ; $i < $saved_index ; i+=1 )); do
 	for (( j=0 ; $j < $available_index ; j+=1 )); do
 		if [[ ${saved[$i]} = ${available[$j]} ]]; then
-			ssid_connectee="${saved[$i]}"
-			break 2
+			recognised_ssids[${recognised_ssids_index}]="${saved[$i]}"
+			recognised_ssids_index+=1
 		fi
 	done
 done
 
-if [[ -z $ssid_connectee ]]; then
+if [[ ${#recognised_ssids[@]} -eq 0 ]]; then
 	wifi_log "no saved network available"
 	exit 1
+elif [[ ${#recognised_ssids[@]} -eq 1 ]]; then
+  	connectee_ssid="${recognised_ssids[0]}"
+else
+  	wifi_log "recognised networks:"
+  	for (( i=0; $i < ${recognised_ssids_index}; i+=1 )); do
+    		printf "\t${i}: ${recognised_ssids[$i]}\n"
+	done
+  read -p "which to connect? " index
+  connectee_ssid="${recognised_ssids[index]}"
+  if [[ -z ${connectee_ssid} ]]; then
+      wifi_log "need to provide correct index"
+      exit 1
+  fi
 fi
 
 
 # obtain password for given SSID
 
-pass_connectee="$(cat "${wifi_list}" | grep "$ssid_connectee" | tr -s ":::" "\n" | sed -n 2p)"
+connectee_pass="$(cat "${wifi_list}" | grep "${connectee_ssid}" | tr -s ":::" "\n" | sed -n 2p)"
 
-wifi_log "connecting to $ssid_connectee"
-wpa_supplicant -i "$wireless_interface" -c <(wpa_passphrase "$ssid_connectee" "$pass_connectee") &
+wifi_log "connecting to ${connectee_ssid}"
+wpa_supplicant -i "$wireless_interface" -c <(wpa_passphrase "${connectee_ssid}" "${connectee_pass}") &
--- _disconnect.sh
+++ _disconnect.sh
@@ -2,3 +2,10 @@
 
 source "/home/sami/dev/bash/wifi/aux.sh"
 
+wifi_log "putting down the network interface"
+ip link set "${wireless_interface}" down
+wifi_log "killing wpa_supplicant processes"
+sudo killall wpa_supplicant
+wifi_log "killing dhcpcd processes"
+sudo killall dhcpcd
+
